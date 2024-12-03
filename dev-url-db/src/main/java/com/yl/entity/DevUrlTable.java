cf34b3a2ba6b7b65c24780ec60db2ea9e6dc2a8e
andre.lan <andre.lan@jtexpress.com>
2024-12-03 15:36:38 +0800

    idGenerator

--- dev-url-db/src/main/java/com/yl/entity/DevUrlTable.java
+++ dev-url-db/src/main/java/com/yl/entity/DevUrlTable.java
@@ -1,8 +1,5 @@
 package com.yl.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.KeySequence;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -13,12 +10,9 @@ import java.time.LocalDateTime;
  */
 @Data
 @TableName("DEV_URL_TABLE")
-@KeySequence(value = "DEV_URL_INC", clazz = Long.class)
 public class DevUrlTable {
 
-    @TableId(value = "id", type = IdType.INPUT)
     private Long id;
-
     private String uri;
     private String tables;
     private String crossPath;
